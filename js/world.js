c49eeb0509c2891118e5f641782a0cdcf5a2a2c4
noelclarkwalker <noelshops@gmail.com>
2026-08-04 23:32:15 -0400

    Batch 2 - Motion Foundation

--- js/world.js
+++ js/world.js
@@ -1,1 +1,32 @@
-// World engine
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loading-screen");
+
+    setTimeout(() => {
+
+        loader.style.opacity = "0";
+        loader.style.pointerEvents = "none";
+
+        setTimeout(() => {
+
+            loader.remove();
+
+        },800);
+
+    },900);
+
+});
+
+document.addEventListener("mousemove",(e)=>{
+
+    document.documentElement.style.setProperty(
+        "--mouseX",
+        e.clientX + "px"
+    );
+
+    document.documentElement.style.setProperty(
+        "--mouseY",
+        e.clientY + "px"
+    );
+
+});
